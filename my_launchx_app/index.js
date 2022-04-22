e997ded3d9ab08730a97dee4b34ad3145e9d3ddc
Axel Reyes <rbaxel.f@gmail.com>
2022-04-21 23:56:33 -0500

    instanciando objetos e imprimiendolos

--- my_launchx_app/index.js
+++ my_launchx_app/index.js
@@ -2,3 +2,13 @@
 
 //importando clase
 const MissionCommander = require('./app/missionCommander')
+
+//instanciando objetos
+
+const linus = new MissionCommander("Linus");
+const assimov = new MissionCommander("Assimov");
+const nikola = new MissionCommander("Nikola");
+//imprimiendo objetos
+console.log(linus.name);
+console.log(assimov.name);
+console.log(nikola.name);
