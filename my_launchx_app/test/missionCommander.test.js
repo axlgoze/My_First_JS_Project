43be7993a7ca47e19dd74944bea9329e790528fc
Axel Reyes <rbaxel.f@gmail.com>
2022-04-22 00:04:41 -0500

    importando clase a prueba

--- my_launchx_app/test/missionCommander.test.js
+++ my_launchx_app/test/missionCommander.test.js
@@ -1,3 +1,5 @@
+//importando clase
+const MissionCommander = require('./../app/missionCommander');
 describe("Esto es una suite de pruebas", () => {
 	test('Caso de prueba 1', () => {
 		const result = 1 + 2
